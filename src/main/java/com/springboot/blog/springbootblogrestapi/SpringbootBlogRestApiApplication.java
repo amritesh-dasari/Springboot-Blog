d25142c6670a40dd38f30c0c9a20bda188d57e34
Amritesh Dasari <dmamritesh@gmail.com>
2024-03-18 19:20:31 -0500

    Adding Info to the swagger-ui

--- src/main/java/com/springboot/blog/springbootblogrestapi/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,32 @@
 package com.springboot.blog.springbootblogrestapi;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot based Blog App REST APIs",
+				description = "My introductory project into learning Spring Boot.",
+				version = "V1.0",
+				contact = @Contact(
+						name = "Mohan Amritesh Dasari",
+						email = "dmamritesh@gmail.com",
+						url = "https://amritesh-dasari.github.io/"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot based Blog App Codebase",
+				url = "https://github.com/amritesh-dasari/Springboot-Blog"
+		)
+)
 public class SpringbootBlogRestApiApplication {
 
 	@Bean
